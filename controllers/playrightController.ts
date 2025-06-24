23cc301cd9bd8786470e35844c6123007f7b9854
gandalfg <tasos.valtinos@gmail.com>
2025-06-24 16:41:21 +0200

    trying it out as an API

--- controllers/playrightController.ts
+++ controllers/playrightController.ts
@@ -3,15 +3,23 @@ import { MCPAgent, MCPClient } from 'mcp-use'
 import { Request, Response } from 'express'
 import 'dotenv/config'
 
+const theQuery = `I want you to go to this website: https://mcp.so/server/MiniMax-MCP/MiniMax-AI?tab=tools , 
+// scrape it and return me back the server configuration JSON, so I can properly set it up in cursor.`
+
 export async function main(req: Request, res: Response) {
   // 1. Configure MCP servers
   try {
     const config = {
       mcpServers: {
-        tavily: { command: 'npx', args: ["-y",
-            "tavily-mcp"], env: {TAVILY_API_KEY: process.env.TAVILY_API_KEY}},
-        airbnb: { command: 'npx', args: ['@openbnb/mcp-server-airbnb'] },
-        playwright: { command: 'npx', args: ['@playwright/mcp@0.0.28'] }
+        // tavily: { command: 'npx', args: ["-y",
+        //     "tavily-mcp"], env: {TAVILY_API_KEY: process.env.TAVILY_API_KEY}},
+        // airbnb: { command: 'npx', args: ['@openbnb/mcp-server-airbnb'] },
+        // playwright: { command: 'npx', args: ['@playwright/mcp@0.0.28'] },
+        firecrawl: { command: 'npx', args: ['-y', 'firecrawl-mcp'], 
+          env : {
+            "FIRECRAWL_API_KEY": "fc-43227f0a42f44c73be828036c27a26f3",
+            "PATH": `${process.env.PATH}`
+          }}
       }
     }
     const client = MCPClient.fromDict(config)
@@ -23,7 +31,7 @@ export async function main(req: Request, res: Response) {
     const agent = new MCPAgent({ llm, client, maxSteps: 20 })
   
     // 4. Run query
-    const result = await agent.run('Give me airbnb options in Peniche, Portugal, then give me the cheapest one..')
+    const result = await agent.run(theQuery)
     console.log('Result:', result)
     res.status(200).json(result)
   } catch (error: any) {
